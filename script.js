8e6997f25709dc98d90daee6ee6424a63cfc0e7c
MxCabrera <53961785+MxCabrera@users.noreply.github.com>
2020-05-11 22:24:18 -0400

    added new stuff

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('it works');
+// CommonJS style
+let kute = require("kute.js"); //grab the core
+require("kute.js/kute-svg"); // Add SVG Plugin
+require("kute.js/kute-css"); // Add CSS Plugin
+require("kute.js/kute-attr"); // Add Attributes Plugin
+require("kute.js/kute-text"); // Add Text Plugin
+
+// AMD style
+define([
+   "kute.js",
+   "kute.js/kute-svg.js", // optional for SVG morph, draw and other SVG related CSS
+   "kute.js/kute-css.js", // optional for additional CSS properties
+   "kute.js/kute-attr.js", // optional for animating presentation attributes
+   "kute.js/kute-text.js" // optional for string write and number incrementing animations
+], function (KUTE) {
+   // ...
+});
