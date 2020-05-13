5a66ae4290215a8db12640f6040284df71e6d4a0
MxCabrera <53961785+MxCabrera@users.noreply.github.com>
2020-05-13 12:16:56 -0400

    fixed styling on header and tried new things

--- script.js
+++ script.js
@@ -2,53 +2,51 @@ const app = {};
 
 app.isPlaying = undefined;
 
+$('.arrowU').on('click', function(){
+   window.scrollTo(0,0);
+});
+
+$('.arrowD').on('click', function(){
+   window.scrollTo(0, 100000);
+});
+
 
 $('.headerButton').on('click', function() {
    window.scrollTo(0,800);
    if (app.isPlaying) {
-      audio.stop()
+      audio.stop();
    } else {
       app.isPlaying = true;
       let audio = new Audio('./assets/intro.mp3');
       audio.volume = 0.01;
       audio.play();
-   }
-})
+   };
+});
 
 
 $(document).ready(function () {
    // Add smooth scrolling to all links
    $("a").on('click', function (event) {
-
       // Make sure this.hash has a value before overriding default behavior
       if (this.hash !== "") {
          // Prevent default anchor click behavior
          event.preventDefault();
-
          // Store hash
          var hash = this.hash;
-
          // Using jQuery's animate() method to add smooth page scroll
          // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
          $('html, body').animate({
             scrollTop: $(hash).offset().top
          }, 800, function () {
-
-            // Add hash (#) to URL when done scrolling (default click behavior)
             window.location.hash = hash;
          });
-      } // End if
+      } 
    });
 });
 
 AOS.init({
-   offset: 0,
+   offset: 250,
    duration: 900
 });
 
-console.log('hey')
-
-let choreographer = new Choreographer(config)
-choreographer.runAnimationsAt(position)
-
-console.log(choreographer)
+console.log('hey');
